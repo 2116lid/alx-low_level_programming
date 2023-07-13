60f398e49dbf21176989ec6eaf839f6abc6a7899
Noah Alemayehu <noahlem21@gmail.com>
2023-07-13 02:59:40 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * this is a program that uses puts
+ * main - this is a program that uses puts
  *Return:0 (succsses)
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
