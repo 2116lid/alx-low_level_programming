956126360944b6cb0fa2073df698a2416365bcc1
Noah Alemayehu <noahlem21@gmail.com>
2023-07-13 02:51:03 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * this is a program that uses puts
+ *Return:0 (succsses)
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
