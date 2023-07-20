ed6232f2639287df94132d190f18fc800448d8c4
Noah Alemayehu <noahlem21@gmail.com>
2023-07-20 11:00:31 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,25 +6,25 @@
  */
 void print_triangle(int size)
 {
-	if (size <= 0)
-	{
-		_putchar('\n');
-	} else
-	{
-		int i;
-		int j;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int i, j;
 
-		for (i = 0; i <= size; i++)
-		{
-			for (j = 0; j < size; j++)
-			{
-			_putchar(' ');
-			}
-			for (j = 1; j <= i; j++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
-	}
+for (i = 1; i <= size; i++)
+{
+for (j = i; j < size; j++)
+{
+_putchar(' ');
+}
+for (j = 1; j <= i; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
 }
