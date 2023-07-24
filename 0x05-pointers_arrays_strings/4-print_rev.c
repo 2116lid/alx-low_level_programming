b1dc19f73623ea1529266e80570cf6eab7481c55
Noah Alemayehu <noahlem21@gmail.com>
2023-07-24 14:18:31 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: Nothing.
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int a;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+
+	s--;
+	for  (a = i; a > 0; a--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
