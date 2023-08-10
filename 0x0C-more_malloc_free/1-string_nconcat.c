8b362c55e92c9a38f333370d21aa8aef3d7eaed9
Noah Alemayehu <noahlem21@gmail.com>
2023-08-10 15:02:09 -0700

    1-string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,8 +17,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	k = strlen(s1);
 	l = strlen(s2);
 
-	if (n >= l)
-		n = l;
 	if (n < l)
 	{
 		str = malloc((k + n + 1) * sizeof(char));
@@ -27,10 +25,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (str == NULL)
 		return (NULL);
-	strncpy(str, s1, k);
-	strncpy(str + k, s2, n);
+	if (s1 != NULL)
+		strcpy(str, s1);
+	else
+		*str = '\0';
+	strncat(str, s2, n);
 
-	str[k + n] = '\0';
-
-	return (str);
+	return(str);
 }
