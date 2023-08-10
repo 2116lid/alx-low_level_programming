9a3eb51c1918d78027619f9a4379377a028df171
Noah Alemayehu <noahlem21@gmail.com>
2023-08-10 14:35:46 -0700

    1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * *string_nconcat - Entry point.
+ * @s1: string number one.
+ * @s2: string number two.
+ * @n:first bytes of s2.
+ *
+ * Return: a pointer.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int k, l;
+	char *str;
+
+	k = strlen(s1);
+	l = strlen(s2);
+
+	if (n >= l)
+		n = l;
+	str = malloc((k + n + 1) * sizeof(char));
+
+	if (str == NULL)
+		return (NULL);
+	strncpy(str, s1, k);
+	strncpy(str + k, s2, n);
+
+	str[k + n] = '\0';
+
+	return (str);
+}
