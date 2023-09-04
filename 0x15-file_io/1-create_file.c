8503d1b968f1d2da3cca21818495a843fa61432e
Noah Alemayehu <noahlem21@gmail.com>
2023-09-04 15:54:42 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,22 +13,21 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, k, wri;
 
-	k = strlen(text_content);
-
 	if (filename == NULL)
 		return (-1);
+	if (text_content != NULL)
+		k = strlen(text_content);
+
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		wri = write(fd, text_content, k);
+	wri = write(fd, text_content, k);
 
-		if (wri == -1)
-			return (-1);
-	}
+	if (wri == -1)
+		return (-1);
+
 	close(fd);
 	return (1);
 }
