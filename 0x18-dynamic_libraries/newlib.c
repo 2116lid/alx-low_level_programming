6a74454ea95e2984c9832f3ab72f30073f8eab4a
Noah Alemayehu <noahlem21@gmail.com>
2023-10-16 04:22:28 -0700

    100-operations.so

---
+++ 0x18-dynamic_libraries/newlib.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+/**
+ * add - a function that adds numbers.
+ * @a: first number.
+ * @b: second number.
+ * Return: sum of the numbers.
+ *
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - a function that subtracts numbers.
+ * @a: first number.
+ * @b: second number.
+ * Return: difference of the numbers.
+ *
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - a function that multiplies numbers.
+ * @a: first number.
+ * @b: second number.
+ * Return: multiplication of the numbers.
+ *
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - a function that divides numbers.
+ * @a: first number.
+ * @b: second number.
+ * Return: division of the numbers.
+ *
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - a function that subtracts numbers.
+ * @a: first number.
+ * @b: second number.
+ * Return: differebnce of the numbers.
+ *
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
